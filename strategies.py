de3b1fa90dcbab80d55c13eb3ac8e3d02a161b0e
Leonardo <54992907+lyonms2@users.noreply.github.com>
2025-09-07 00:48:30 -0300

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -2,21 +2,17 @@ import pandas as pd
 
 def strategy_ema_hull_cross(df):
     """
-    Sinal de compra: Preço fechado da vela anterior acima da EMA50 e acima da HULL.
-    Sinal de venda: Preço fechado da vela anterior abaixo da EMA50 e abaixo da HULL.
+    Sinal de compra: Preço fechado da vela anterior acima da EMA55 e acima da HULL55.
+    Sinal de venda: Preço fechado da vela anterior abaixo da EMA55 e abaixo da HULL55.
     A análise é feita na penúltima vela (última fechada).
     """
     if len(df) < 2:
         return "-"
-
     close_prev = df['close'].iloc[-2]
-    ema_prev = df['EMA50'].iloc[-2]
+    ema_prev = df['EMA55'].iloc[-2]
     hull_prev = df['HULL55'].iloc[-2]
-
-    # Sinal de compra
     if close_prev > ema_prev and close_prev > hull_prev:
         return "Compra"
-    # Sinal de venda
     elif close_prev < ema_prev and close_prev < hull_prev:
         return "Venda"
     else:
